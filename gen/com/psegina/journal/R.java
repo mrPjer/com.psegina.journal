5a24fe6571d14ec1d816510e936fc8da2aaa8616
Petar Šegina <psegina@ymail.com>
2010-11-07 18:02:13 +0100

    Some work in main layout, just for testing

--- gen/com/psegina/journal/R.java
+++ gen/com/psegina/journal/R.java
@@ -22,6 +22,7 @@ public final class R {
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int quickinput=0x7f030001;
+        public static final int singlejournalentry=0x7f030002;
     }
     public static final class string {
         public static final int MainNewButtonLabel=0x7f040007;
