63cb9c91f5b5d98a43257d5a3fea738135022bc1
Petar Šegina <psegina@ymail.com>
2010-11-07 12:52:06 +0100

    Rewrote some parts of JournalEntry

--- src/com/psegina/journal/JournalEntry.java
+++ src/com/psegina/journal/JournalEntry.java
@@ -3,33 +3,48 @@ package com.psegina.journal;
 import android.database.Cursor;
 
 public class JournalEntry {
-	private long timestamp;
-	private String message;
-	private String category;
+	private long mTimestamp;
+	private String mBody;
+	private String mTags;
 	
 	/**
-	 * Constructs a new general JournalEntry from the message 
-	 * @param msg The message to include in the entry
+	 * Constructs a new JournalEntry.
+	 * Note that you must set a body with setBody before
+	 * using submit if you use this constructor.
 	 */
-	public JournalEntry(String msg){
-		timestamp = System.currentTimeMillis()/1000;
-		message = msg;
+	public JournalEntry(){
+		setTimestamp();
 	}
 	
 	/**
-	 * Assigns the entry to a category
-	 * @param cat The category to assign the entry to
+	 * Constructs a new general JournalEntry from a body 
+	 * @param body The message to include in the entry
 	 */
-	public void assignCategory(int cat){
-		//TODO
+	public JournalEntry(String body){
+		setTimestamp();
+		mBody = body;
 	}
 	
 	/**
-	 * 
-	 * @return Displays all the data related to the entry in a human readable format
+	 * Constructs a JournalEntry using supplied arguments.
+	 * An entry constructed like this can immediately be
+	 * submitted using submit.
+	 * @param body The body of the entry
+	 * @param tags The tagline of the entry
+	 */
+	public JournalEntry(String body, String tags){
+		setTimestamp();
+		mBody = body;
+		mTags = tags;
+	}
+		
+	/**
+	 * If you need to get the JournalEntry in a human readable format for debug purposes,
+	 * simply write out the result of this function.
+	 * @return String representing all the data related to the entry in a human readable format
 	 */
 	public String getDescriptor(){
-		return "("+timestamp+")"+category+"::"+message;
+		return "("+mTimestamp+")"+mTags+"::"+mBody;
 	}
 	
 	/**
@@ -48,6 +63,29 @@ public class JournalEntry {
 	public static Cursor getEntries(){
 		// TODO
 		 return (Cursor) null;
+	}
+	
+	/**
+	 * Sets the timestamp of the entry to current time in seconds
+	 */
+	private void setTimestamp(){
+		mTimestamp = System.currentTimeMillis()/1000;
+	}
+	
+	/**
+	 * Sets the body value of the entry
+	 * @param body The body of the entry to record
+	 */
+	public void setBody(String body){
+		mBody = body;
+	}
+	
+	/**
+	 * Sets the tagline of the entry
+	 * @param tags The tagline to apply
+	 */
+	public void setTags(String tags){
+		mTags = tags;
 	}
 	
 }
