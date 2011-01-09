3beb880229824e8b076655a15f433a412b0f61ba
Petar Šegina <psegina@ymail.com>
2011-01-09 21:33:42 +0100

    EntriesCursorAdapter :: Completed header groupation

--- src/com/psegina/journal/data/EntriesCursorAdapter.java
+++ src/com/psegina/journal/data/EntriesCursorAdapter.java
@@ -1,11 +1,7 @@
-/**
- * 
- */
 package com.psegina.journal.data;
 
 import java.text.DateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 import android.content.Context;
 import android.database.Cursor;
@@ -13,19 +9,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 import com.psegina.journal.R;
 
 /**
+ * A CursorAdapter that takes a cursor pointing
+ * to JournalEntries and creates a suitable View
+ * from it.
  * @author Petar Šegina <psegina@ymail.com>
- *
+ * 
  */
 public class EntriesCursorAdapter extends CursorAdapter {
 	private LayoutInflater mInflater;	
+	private int current;
+	private int previous;
+	private Calendar calendar;
 
 	public EntriesCursorAdapter(Context context, Cursor c, boolean requery) {
 		super(context, c, requery);
 		mInflater = LayoutInflater.from(context);
+		calendar = Calendar.getInstance();
 	}
 
 	@Override
@@ -39,32 +43,35 @@ public class EntriesCursorAdapter extends CursorAdapter {
 		JournalEntry.ViewBuilder builder = new JournalEntry.ViewBuilder();
 		builder.setDateFormat(DateFormat.FULL);
 		view = builder.populateView(view, JournalEntry.Builder.fromCursor(cursor));
+		if( !isNewGroup(cursor) )
+			( (TextView) view.findViewById(R.id.JournalEntry_Timestamp) ).setVisibility(View.GONE);
+		else
+			( (TextView) view.findViewById(R.id.JournalEntry_Timestamp) ).setVisibility(View.VISIBLE);
+		
 	}
 	
-	private boolean isNewGroup(Cursor cursor, int position){
-		Calendar calendar = Calendar.getInstance();
-		//Log.e("POS", ""+position);
-		int current = 0;
-		int previous = 0;
-		calendar.setTime(new Date(cursor.getLong(cursor.getColumnIndex(Database.KEY_TIMESTAMP)) * 1000));
-		current =  new Integer(calendar.get(Calendar.DAY_OF_MONTH)) ;
-		//Log.e("CUR", ""+current);
-		if(cursor.getPosition() > 0){
-		cursor.moveToPosition(position - 1);
-		calendar.setTime(new Date(cursor.getLong(cursor.getColumnIndex(Database.KEY_TIMESTAMP)) * 1000));
-		previous = calendar.get(Calendar.DAY_OF_MONTH);
-		//Log.e("PRE", ""+previous);
-		//Log.e("CUR2", ""+ current);
-		cursor.moveToPosition(position);
-		if( previous != current )
-			return true;
-		else
-			return false;
+	/**
+	 *
+	 * @param cursor A cursor pointing to a specific JournalEntry in the database
+	 * @return true if the data pointed by Cursor belongs to a new group, false otherwise
+	 */
+	private boolean isNewGroup(Cursor cursor){
+		calendar.setTimeInMillis( cursor.getLong(cursor.getColumnIndex(Database.KEY_TIMESTAMP)) * 1000);
+		current =  calendar.get(Calendar.DAY_OF_YEAR);
+
+		if( !cursor.isFirst() ){
+			cursor.moveToPrevious();
+			calendar.setTimeInMillis( cursor.getLong(cursor.getColumnIndex(Database.KEY_TIMESTAMP)) * 1000);
+			previous = calendar.get(Calendar.DAY_OF_YEAR);
+			cursor.moveToNext();
+			if( previous != current )
+				return true;
+			else
+				return false;
 		}
 		else
-		return true;
+			return true;
+		
 	}
 
-
-	
 }
